1b0a3abf72c8fe3cadf45591d52071284d92a2b9
Jorge Nascimento <nascimento.dev.io@gmail.com>
2022-06-09 11:30:22 -0300

    change icon location

--- src/components/MapLocation/index.js
+++ src/components/MapLocation/index.js
@@ -1,27 +1,32 @@
 import React, { useContext } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
 
 import { IPTrackContext } from "../../contexts/TrackContext";
 
-import iconLocation from "../../assets/images/icon-location.svg";
-
 import { Container } from "./styles";
+
+const myIcon = (_iconSize) => {
+  return L.icon({
+    iconUrl: require("../../assets/images/icon-location.png"),
+    iconSize: _iconSize,
+  });
+};
 
 function UpdateMapCenter(props) {
   const map = useMap();
-  map.panTo(props.mapCenter);
+  map.flyTo(props.mapCenter);
   return null;
 }
 
 export const MapLocation = () => {
   const { coordinates, infosTrack } = useContext(IPTrackContext);
 
-  console.log(iconLocation);
   return (
     <Container>
       <MapContainer
         center={[40.712776, -74.005974]}
-        zoom={10}
+        zoom={13}
         scrollWheelZoom={true}
         style={{ height: "70vh", zIndex: -1 }}
       >
@@ -30,7 +35,7 @@ export const MapLocation = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={coordinates}>
+        <Marker position={coordinates} icon={myIcon([30, 30])}>
           <Popup>
             Localidade: {infosTrack.location} <br />
           </Popup>
